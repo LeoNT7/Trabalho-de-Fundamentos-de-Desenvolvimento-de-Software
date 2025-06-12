0b683f606615dd1ba8b27adab222eb5effb9bb88
Leo <lbento026@gmail.com>
2025-06-11 22:26:44 -0300

    Adicionando todos os arquivos do projeto

---
+++ script.js
@@ -0,0 +1,6 @@
+  const botao = document.getElementById("botao");
+  const paragrafo = document.getElementById("mensagem");
+
+  botao.onclick = function() {
+    paragrafo.innerHTML = "📚 Você sabia? O livro mais vendido e traduzido do mundo (depois da Bíblia) é O Pequeno Príncipe, escrito por Antoine de Saint-Exupéry. Ele já foi traduzido para mais de 500 idiomas e dialetos, incluindo até linguagem de sinais e braile! 🤯💫";
+  };
